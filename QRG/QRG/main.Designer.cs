bc70997916cb939b823a89714b444d1b554cd2f2
unknown <xor3160@email.daelim.ac.kr>
2022-12-04 00:08:40 +0900

    First commit

---
+++ QRG/QRG/main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace QRG
+{
+    partial class main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QRpic = new System.Windows.Forms.PictureBox();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.QRpic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // QRpic
+            // 
+            this.QRpic.BackColor = System.Drawing.Color.White;
+            this.QRpic.Location = new System.Drawing.Point(150, 36);
+            this.QRpic.Name = "QRpic";
+            this.QRpic.Size = new System.Drawing.Size(300, 300);
+            this.QRpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.QRpic.TabIndex = 0;
+            this.QRpic.TabStop = false;
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdress.Location = new System.Drawing.Point(108, 359);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(342, 29);
+            this.txtAdress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Address: ";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(456, 355);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(116, 37);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(150, 407);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(150, 36);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(300, 407);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 36);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.QRpic);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Code Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.QRpic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox QRpic;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ QRG/QRG/main.cs
@@ -0,0 +1,86 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRG
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+            btnGenerate.Click += BtnGenerate_Click;
+            btnCopy.Click += BtnCopy_Click;
+            btnSave.Click += BtnSave_Click;
+        }
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            if (txtAdress.Text.Equals(""))
+            {
+                MessageBox.Show("You should type the Address first!", "Error");
+            }
+            else
+            {
+                try
+                {
+                    QRCodeGenerator qr = new QRCodeGenerator();
+                    QRCodeData data = qr.CreateQrCode(txtAdress.Text, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrcode = new QRCode(data);
+
+                    QRpic.Image = qrcode.GetGraphic(20);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error");
+                }
+            }
+        }
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            if (QRpic.Image == null)
+            {
+                MessageBox.Show("You should generate QR Code First!", "Error");
+            }
+            else
+            {
+                try
+                {
+                    Clipboard.SetImage(QRpic.Image);
+                    MessageBox.Show("Successfully copied to clipboard!", "Copied");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error");
+                }
+            }
+        }
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (QRpic.Image == null)
+            {
+                MessageBox.Show("You should generate QR Code First!", "Error");
+            }
+            else
+            {
+                try
+                {
+                    string filePath = Application.StartupPath;
+                    QRpic.Image.Save(filePath + "\\qrcode.jpg", ImageFormat.Jpeg);
+                    MessageBox.Show($"Successfully Saved image:\n{filePath}\\qrcode.jpg", "Saved");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error");
+                }
+            }
+        }
+    }
+}
